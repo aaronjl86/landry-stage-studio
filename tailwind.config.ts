8bc77dbbc92e61dfe12c10ca730500c24387c7fa
ThatAaronGuy <55515234+aaronjl86@users.noreply.github.com>
2025-11-16 00:32:35 -0800

    Change darkMode setting to 'media'

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,7 +2,7 @@ import type { Config } from "tailwindcss";
 import tailwindcssAnimate from "tailwindcss-animate";
 
 export default {
-  darkMode: false,
+  darkMode: 'media',
   content: ["./pages/**/*.{ts,tsx}", "./components/**/*.{ts,tsx}", "./app/**/*.{ts,tsx}", "./src/**/*.{ts,tsx}"],
   prefix: "",
   theme: {
